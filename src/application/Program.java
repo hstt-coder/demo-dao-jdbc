869fd1d6bf1c919853ae907b44edbafc430a8324
hstt-coder <eduardolopes19@outlook.com>
2022-07-09 23:28:33 -0300

    delete impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DAOFactory;
 import model.dao.SellerDAO;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {		
+		Scanner sc = new Scanner(System.in);
+		
 		
 		SellerDAO sellerDAO = DAOFactory.createSellerDAO();
 		
@@ -36,12 +39,22 @@ public class Program {
 		sellerDAO.insert(seller4);
 		System.out.println("INSERTED! id = " + seller4.getId());
 		
-		System.out.println("\n====== TEST 4: seller update =======");
+		System.out.println("\n====== TEST 5: seller update =======");
 		seller = sellerDAO.findById(1);
 		seller.setName("Martha Waine");
 		seller.setBaseSalary(40343.34);
 		sellerDAO.update(seller);
 		System.out.println("Updated!");
 		
+		System.out.println("\n====== TEST 6: seller delete =======");
+		System.out.println("Enter id for delete test");
+		int id = sc.nextInt();
+		sellerDAO.deleteById(id);
+		System.out.println("Deleted!");
+		
+		
+		
+	
+		sc.close();
 	}
 }
